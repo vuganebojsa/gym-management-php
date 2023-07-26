36d914be88a8d21eb675f73fe73c06a004dc32bb
nebojsavuga <vuga.sv53.2020@uns.ac.rs>
2023-07-26 11:26:43 +0200

    Implemented login and redirection to admin page

--- index.php
+++ index.php
@@ -1,17 +1,80 @@
+<?php
+
+    require_once 'config.php';
+    if(isset($_SESSION['admin_id'])){
+        header('location: admin_dashboard.php');
+    }
+    if($_SERVER['REQUEST_METHOD'] == "POST"){
+
+        $username = $_POST['username'];
+        $password = $_POST['password'];
+
+        $sql = "select admin_id, password from admins where username = ?";
+
+        $run = $conn->prepare($sql);
+        $run->bind_param("s", $username);
+        $run->execute();
+
+        $results = $run->get_result();
+        if($results->num_rows == 1){
+            $admin = $results->fetch_assoc();
+           
+            if(password_verify($password, $admin['password'])){
+                $_SESSION['admin_id'] = $admin['admin_id'];
+                header('location: admin_dashboard.php');
+            }
+            else{
+                $_SESSION['error'] = "Username ili lozinka su neispravni.";
+                header('location: index.php');
+                exit;
+            }
+
+        }else{
+            $_SESSION['error'] = "Username ili lozinka su neispravni.";
+            header('location: index.php');
+            exit;
+        }
+
+    }
+?>
+
 <!DOCTYPE html>
 <html lang="en">
   <head>
     <meta charset="utf-8" />
-    <link rel="icon" href="%PUBLIC_URL%/favicon.ico" />
+    <link rel="icon" href="assets/gym.png" />
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <meta name="viewport" content="width=device-width, initial-scale=1" />
 
-    <title>Gym Management</title>
+    <title>Gym Management Login</title>
   </head>
 <body>
-   
+<?php
 
+    if(isset($_SESSION['error'])){
+        echo $_SESSION['error'] . "<br>";
+        unset($_SESSION['error']);
+    }
+?>
 
+<form action="" method="post">
+    <div class="row text-center m-5">
+
+        <div class="col-12 my-2">
+        Username: <input type="text" name="username">
+        </div>
+        <div class="col-12 my-2">
+        Password: <input type="password" name="password">
+
+        </div>
+        <div class="col-12 my-2">
+        <input type="submit" value="Login">
+        
+        </div>
+
+    </div>
+
+</form>
 
 
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
---
+++ insert_values_to_db_script.php
@@ -0,0 +1,17 @@
+
+<?php
+    require_once 'config.php';
+
+    $username = "nebojsa";
+    $password = "Nebojsa123";
+    $hashed_pw = password_hash($password, PASSWORD_DEFAULT);
+
+    $sql = "insert into admins(username, password) values(?, ?)";
+
+    $run = $conn->prepare($sql);
+    $run->bind_param("ss", $username, $hashed_pw);
+
+    $run->execute();
+
+    
+?>
